5088ecdcc61159f43dd83d7e3aabd900d52da29c
davis67 <davisag67@gmail.com>
2020-11-04 08:12:16 +0300

    add posts to the application

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at",)
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "description",)
--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,57 @@
 from django.db import models
+from core import models as core_models
 
 
-class Post(models.Model):
+class Category(core_models.TimeStampedModel):
+    """ Category Model Definition """
+
+    name = models.CharField(max_length=140)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition"""
+
+    caption = models.CharField(max_length=100)
+    file = models.ImageField()
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Comment(core_models.TimeStampedModel):
+    """ Comment Model Definition"""
+
+    description = models.TextField()
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+class Reply(core_models.TimeStampedModel):
+    """ Reply Model Definition"""
+
+    description = models.TextField()
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+class Post(core_models.TimeStampedModel):
     """ Post Model Definition """
 
     title = models.CharField(max_length=140)
     description = models.TextField()
+    author = models.ForeignKey(
+        "users.User", related_name="author", on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)
+    authorized_by = models.ForeignKey(
+        "users.User", related_name="authorized_by", on_delete=models.SET_NULL, blank=True, null=True)
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE)
